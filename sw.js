ed2fcb03db6cfac1909f0c5230d8086134901275
amaaa0hhh <txdrxd@gmail.com>
2026-06-22 03:48:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* iso-current PWA service worker
    方針: ファイル名に依存しないランタイムキャッシュ。
    初回オンライン時にfetchしたものをキャッシュし、以降はオフラインでもキャッシュから配信する。 */
-const CACHE = 'iso-current-v2';
+const CACHE = 'iso-current-v3';
 
 self.addEventListener('install', function (e) {
   // すぐ有効化
